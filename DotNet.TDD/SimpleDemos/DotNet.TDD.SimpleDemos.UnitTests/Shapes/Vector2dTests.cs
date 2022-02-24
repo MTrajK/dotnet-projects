1b36e82d189504ab8827055d760ba2648ec2acd8
Meto Trajkovski <7501012+MTrajK@users.noreply.github.com>
2022-02-24 01:41:40 +0100

    Added shapes example

---
+++ DotNet.TDD/SimpleDemos/DotNet.TDD.SimpleDemos.UnitTests/Shapes/Vector2dTests.cs
@@ -0,0 +1,23 @@
+﻿using DotNet.TDD.SimpleDemos.Shapes;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.TDD.SimpleDemos.UnitTests.Shapes
+{
+    public class Vector2dTests
+    {
+        [Test]
+        public void Add_X4Y2_X5Y4()
+        {
+            // Arrange
+            var vector = new Vector2d(1, 2);
+            var addVector = new Vector2d(4, 2);
+
+            // Act
+            var result = vector.Add(addVector);
+
+            // Assert
+            result.Should().BeEquivalentTo(new Vector2d(5, 4));
+        }
+    }
+}
