f603feb93c7fcd513818efd4948f5424c6281604
Meto Trajkovski <7501012+MTrajK@users.noreply.github.com>
2025-11-19 12:49:37 +0100

    Added Payments tests

---
+++ DotNet.MeaningfulUnitTests/Tests/Payments/DotNet.MeaningfulUnitTests.Payments.TDD.UnitTests/Services/PaymentServiceTests.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNet.MeaningfulUnitTests.Payments.TDD.UnitTests.Services
+{
+    using DotNet.MeaningfulUnitTests.Payments.TDD.DTOs;
+    using DotNet.MeaningfulUnitTests.Payments.TDD.Services;
+    using DotNet.MeaningfulUnitTests.Payments.TDD.Utils;
+
+    using FluentAssertions;
+
+    using Moq;
+
+    using System.Net;
+
+    [TestClass]
+    public class PaymentServiceTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            // Arrange
+
+            // Act
+           
+            // Assert
+        }
+    }
+}
