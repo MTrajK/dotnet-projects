00ad4b9dbe14ad95a0c6316b39307c4acd62e905
Meto Trajkovski <7501012+MTrajK@users.noreply.github.com>
2026-02-02 22:50:50 +0100

    Added Branching Workflow

--- DotNet.AIAgents/DotNet.AIAgents.Samples/DotNet.AIAgents.Samples.BranchingWorkflow/Program.cs
+++ DotNet.AIAgents/DotNet.AIAgents.Samples/DotNet.AIAgents.Samples.BranchingWorkflow/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.Agents.AI.Workflows;
 Func<string, string> lowercaseFunc = s => s.ToLower();
 var lowercaseExecutor = lowercaseFunc.BindAsExecutor("LowercaseExecutor");
 
+Func<string, string> uppercaseFunc = s => s.Replace("upper", "UPPER");
+var uppercaseExecutor = uppercaseFunc.BindAsExecutor("UpperExecutor");
+
 Func<string, string> reverseFunc = s => new string(s.Reverse().ToArray());
 var reverseExecutor = reverseFunc.BindAsExecutor("ReverseExecutor");
 
@@ -13,17 +16,23 @@ Func<string, string> replaceFunc = s => Regex.Replace(s, @"[^a-zA-Z0-9\s]", "");
 var replaceExecutor = replaceFunc.BindAsExecutor("ReplaceExecutor");
 
 
-// Build the workflow by connecting executors sequentially
+Func<string, bool> GetCondition(bool expectedResult) =>
+    s => s.Contains("upper") == expectedResult;
+
+
+// Build the workflow by connecting executors conditionally
 var workflow = new WorkflowBuilder(lowercaseExecutor)
-    .AddEdge(lowercaseExecutor, reverseExecutor)
+    .AddEdge(lowercaseExecutor, uppercaseExecutor, condition: GetCondition(true))
+    .AddEdge(uppercaseExecutor, reverseExecutor)
+    .AddEdge(lowercaseExecutor, reverseExecutor, condition: GetCondition(false))
     .AddEdge(reverseExecutor, replaceExecutor)
     .WithOutputFrom(replaceExecutor)
     .Build();
 
 
-// Execute the workflow with input data - Try with "Hello, World!"
-Console.WriteLine("The Sequential Workflow is up. Try it.");
-Console.WriteLine("______________________________________");
+// Execute the workflow with input data - Try with "Hello, World!" and "Hello, Upper World!"
+Console.WriteLine("The Conditional Workflow is up. Try it.");
+Console.WriteLine("_______________________________________");
 string message = "";
 while ((message = Console.ReadLine()) != "exit")
 {
